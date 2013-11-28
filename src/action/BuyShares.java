893e9cad635d5b418474bdffb3cfd8f1165ac3e3
joaosimoes <joaomiguel_condeixa@hotmail.com>
2013-11-28 21:53:56 +0000

    Add all actions.

---
+++ src/action/BuyShares.java
@@ -0,0 +1,31 @@
+package action;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: joaosimoes
+ * Date: 11/28/13
+ * Time: 7:23 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class BuyShares extends Client {
+
+    private String idea;
+    private String share_num;
+    private String new_price_share;
+
+    public String execute() {
+        return SUCCESS;
+    }
+
+    public void setIdea(String idea) {
+        this.idea = idea;
+    }
+
+    public void setShare_num(String share_num) {
+        this.share_num = share_num;
+    }
+
+    public void setNew_price_share(String new_price_share) {
+        this.new_price_share = new_price_share;
+    }
+}
---
+++ src/action/Portfolio.java
@@ -0,0 +1,24 @@
+package action;
+
+import common.IdeaInfo;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: joaosimoes
+ * Date: 11/28/13
+ * Time: 7:37 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Portfolio extends Client {
+
+    private IdeaInfo[] ideas;
+
+    public String execute() {
+        return SUCCESS;
+    }
+
+    public IdeaInfo[] getIdeas() {
+        return this.ideas;
+    }
+
+}
